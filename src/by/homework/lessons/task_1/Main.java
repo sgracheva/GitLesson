c6281793612a9a5d461493ca1222782c3aa3320f
sgracheva <svetlanagracheva17@yandex.by>
2025-09-16 00:13:56 +0300

    Homework/task_1

--- src/by/homework/lessons/task_1/Main.java
+++ src/by/homework/lessons/task_1/Main.java
@@ -2,6 +2,6 @@ package by.homework.lessons.task_1;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Gracheva Svetlana, 10.09.2025 21:03");
+        System.out.println("Gracheva Svetlana,10.09.2025 21:03");
     }
 }
