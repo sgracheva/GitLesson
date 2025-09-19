431e3f0eaa397d0d0af33f9b39f7e64199530bee
sgracheva <svetlanagracheva17@yandex.by>
2025-09-19 18:55:32 +0300

    Homework/task_4

--- src/by/homework/lessons/task_4/Operators.java
+++ src/by/homework/lessons/task_4/Operators.java
@@ -5,25 +5,28 @@ import java.util.Scanner;
 public class Operators {
 
     public static void main(String[] args) {
-////       1.На вход даны 3 числа. Вывести в консоль только четные числа
-//        System.out.println("*********Задача 1*************");
-//        Scanner in=new Scanner(System.in);
-//        System.out.println("Введите x");
-//        int x= in.nextInt();
-//        System.out.println("Введите y");
-//        int y= in.nextInt();
-//        System.out.println("Введите z");
-//        int z= in.nextInt();
-//
-//        if (x%2==0)
-//        {
-//            System.out.println("Число " + x + " делится на 2");
-//        }if (y%2==0){
-//            System.out.println("Число " + y + " делится на 2");
-//        }
-//        if (z%2==0){
-//            System.out.println("Число " + z + " делится на 2");
-//        }
+//       1.На вход даны 3 числа. Вывести в консоль только четные числа
+        System.out.println("*********Задача 1*************");
+        Scanner in=new Scanner(System.in);
+        System.out.println("Введите x");
+        int x= in.nextInt();
+        System.out.println("Введите y");
+        int y= in.nextInt();
+        System.out.println("Введите z");
+        int z= in.nextInt();
+
+        if (x%2==0)
+        {
+            System.out.println("Число " + x + " делится на 2");
+        }if (y%2==0){
+            System.out.println("Число " + y + " делится на 2");
+        }
+        if (z%2==0){
+            System.out.println("Число " + z + " делится на 2");
+        }
+        else {
+            System.out.println("Ни одно из трех чисел не является четным");
+        }
 
 //        2. На вход даны 3 числа. Вывести в консоль только те числа, которые делятся на 2 и на 5 (используйте оператор % для проверки деления без остатка)
 
@@ -44,6 +47,8 @@ public class Operators {
         }
         if (z1 % 2 == 0 && z1 % 5 == 0) {
             System.out.println("Число " + z1 + " делится на 2 и на 5");
+        }else {
+            System.out.println("Ни одно из трех чисел не делится на 2 и на 5");
         }
 
         // 3. Дано 15/x=result, где x-число, которое вводится с командной строки,
@@ -52,6 +57,7 @@ public class Operators {
         // "Результат деления равен 5" В других случаях вывести: "Результат деления равен дробному числу".
         //При этом в последнем случае вывести точный результат деления (использовать приведение типов)
         //Для выполнения задания использовать оператор switch- case
+
 
 
     }
