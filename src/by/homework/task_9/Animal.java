ff4ec612ba35c51c7bd62dd174cc66c53ecad432
sgracheva <svetlanagracheva17@yandex.by>
2025-10-15 00:17:46 +0300

    Homework/task_9 1.1-1.2

---
+++ src/by/homework/task_9/Animal.java
@@ -0,0 +1,25 @@
+package by.homework.task_9;
+
+public class Animal {
+    //Поля в классе животное: окрас, максимальная продолжительность жизни, тип еды которой питаются (мясо, насекомые, растения).
+
+    String color;
+    int maxlife;
+    String food;
+
+    public Animal(String color, int maxlife, String food) {
+    }
+
+    // Определить методы в классе животное: (издание звуков, животное играет)
+
+    public void sound () {
+        System.out.println("Животное издает звук: ");
+    }
+
+    public void  play(){
+        System.out.println("Животное играет: ");
+    }
+
+
+
+}
---
+++ src/by/homework/task_9/Bird.java
@@ -0,0 +1,61 @@
+package by.homework.task_9;
+
+public class Bird extends Animal{
+    //Определить поля в классе Птица: семейство, максимальная высота полёта. В классе птица методы-петь, клевать, высиживать птенцов.
+    //При этом, в консоли должны выводиться поля семейства птиц с соответствующим поведением (пример: Воробей поёт).
+    private String familyBird;
+    private int maxflight;
+
+
+    public Bird(String color, int maxlife, String food,String family,int maxflight) {
+        super(color, maxlife, food);
+        this.familyBird=family;
+        this.maxflight=maxflight;
+    }
+
+    public void sing(){
+        System.out.println(familyBird+ " поет");
+    }
+
+    public void peck(){
+        System.out.println(familyBird+ " клюет");
+    }
+
+    public void hatchСhicks(){
+        System.out.println(familyBird+ " высиживает птенцов");
+    }
+
+    @Override
+    public void sound(){
+        System.out.println(familyBird+ " громко поет");
+    }
+
+    @Override
+    public void play(){
+        System.out.println(familyBird+ " играет");
+    }
+
+    @Override
+    public String toString ()
+    {String result= super.toString();
+        result= result + "Семейство: " + familyBird + ", Максимальная высота полёта: " + maxflight+ " м. ";
+        return result;
+
+    }
+
+    public String getFamilyBird() {
+        return familyBird;
+    }
+
+    public void setFamilyBird(String familyBird) {
+        this.familyBird = familyBird;
+    }
+
+    public int getMaxflight() {
+        return maxflight;
+    }
+
+    public void setMaxflight(int maxflight) {
+        this.maxflight = maxflight;
+    }
+}
