4ac8a3831ec02148ff8ee0895ef8ec0938b8085e
sgracheva <svetlanagracheva17@yandex.by>
2025-10-26 21:49:20 +0300

    Homework/task_10 new ver

--- src/by/homework/lessons/task_10/Bird.java
+++ src/by/homework/lessons/task_10/Bird.java
@@ -1,5 +1,7 @@
 package by.homework.lessons.task_10;
 
+import java.util.Objects;
+
 public class Bird extends Animal {
     private String familyBird;
     private int maxflight;
@@ -27,16 +29,33 @@ public class Bird extends Animal {
         this.maxflight = maxflight;
     }
 
+//    public boolean equals(Object obj) {
+//        if (!(obj instanceof Bird)) {
+//            return false;
+//        }
+//        Bird bird = (Bird) obj;
+//
+//        if (familyBird.equals(bird.getFamilyBird()) && maxflight == bird.getMaxflight()) {
+//            return true;
+//        }
+//        return false;
+//    }
+
+
     public boolean equals(Object obj) {
-        if (!(obj instanceof Bird)) {
-            return false;
-        }
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
         Bird bird = (Bird) obj;
 
-        if (familyBird.equals(bird.getFamilyBird()) && maxflight == bird.getMaxflight()) {
-            return true;
-        }
-        return false;
+        if (this.maxlife != bird.maxlife)
+            return false;
+        if (this.maxflight != bird.maxflight)
+            return false;
+        if (!Objects.equals (familyBird, bird.familyBird))
+            return false;
+        return color != null && color.equals(bird.color);
     }
 
     public int hashCode() {
