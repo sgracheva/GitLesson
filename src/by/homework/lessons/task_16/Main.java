358e4c340e3da82bb74025fc89206b3ec8762c8a
sgracheva <svetlanagracheva17@yandex.by>
2025-11-17 17:49:30 +0300

    Homework/task_15

---
+++ src/by/homework/lessons/task_16/Main.java
@@ -0,0 +1,8 @@
+package by.homework.lessons.task_16;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/by/homework/lessons/task_16/NotEnoughMoneyException.java
@@ -0,0 +1,22 @@
+package by.homework.lessons.task_16;
+
+public class NotEnoughMoneyException extends RuntimeException {
+
+    //Создать своё непроверяемое исключение NotEnoughMoneyException.
+    // В этом классе добавьте поле String errorCode, переопределите гетеры и сетеры
+
+    private String errorCode;
+
+    public NotEnoughMoneyException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+}
